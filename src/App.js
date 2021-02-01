f882187c2a862025ca8d79f4afb5762f2c95fa1b
dunsin2018 <dunsin.fakorede@gmail.com>
2021-02-01 14:16:04 +0200

    --- Auto-Git Commit ---

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    monsters: [
+      {
+        name: "Frankenstein",
+        id: "a1",
+      },
+      {
+        name: "Dracula",
+        id: "a2",
+      },
+      {
+        name: "Zombie",
+        id: "a3",
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
