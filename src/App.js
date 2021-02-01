7fd68ed4ebbc38da208e7f4eb778aa19451d85a1
dunsin2018 <dunsin.fakorede@gmail.com>
2021-02-01 15:46:34 +0200

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -15,12 +15,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList name="Dunsin">
-          <h1>Dunsin</h1>
-        </CardList>
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
