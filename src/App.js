61fd2137c7bb7ff8bb23e0be29c5acb4d7b2c314
dunsin2018 <dunsin.fakorede@gmail.com>
2021-02-02 02:12:25 +0200

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { SearchBox } from "./components/search-box/search-box.component";
 
 import { CardList } from "./components/card-list/card-list.component";
 import "./App.css";
@@ -21,10 +22,9 @@ class App extends Component {
     );
     return (
       <div className="App">
-        <input
-          type="search"
+        <searchBox
           placeholder="search monsters"
-          onChange={(e) => this.setState({ searchField: e.target.value })}
+          handleChange={(e) => this.setState({ searchField: e.target.value })}
         />
         <CardList monsters={filteredMonsters} />
       </div>
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./search-box.style.css";
 
-const SearchBox = ({ placeholder, handleChange }) => (
+export const SearchBox = ({ placeholder, handleChange }) => (
   <input
     className="search"
     type="search"
