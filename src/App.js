e89d2ad7141f5bc32283f9b62b810d1ca4c9801a
dunsin2018 <dunsin.fakorede@gmail.com>
2021-02-04 00:12:36 +0200

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -19,6 +19,10 @@ class App extends Component {
     );
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter((monsters) =>
@@ -26,7 +30,11 @@ class App extends Component {
     );
     return (
       <div className="App">
-        <SearchBox placeholder="search monsters" />
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
         <CardList monsters={filteredMonsters} />
       </div>
     );
